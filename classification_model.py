37f75953ad6c9c7b9e7c52c0850fa8c60328b2cd
hb13879 <hb13879@bristol.ac.uk>
2018-06-13 14:45:23 +0100

    added testing

--- classification_model.py
+++ classification_model.py
@@ -4,6 +4,7 @@ import pandas as pd
 def read_in_data():
     return pd.read_csv("train.csv")
 
+
 def drop_categories(train):
     return train.drop(["PassengerId","Survived","Name","Ticket","Cabin"],1)
 
@@ -44,7 +45,8 @@ def initialise_weights(hidden_units,features,output_units):
     W2 = np.random.rand(output_units,hidden_units)
     b1 = np.zeros((hidden_units,1))
     b2 = np.zeros((output_units,1))
-    return W1, W2, b1, b2
+    parameters = {"W1" : W1, "W2" : W2, "b1" : b1, "b2": b2}
+    return parameters
 
 def feedforward(Xtrain,parameters):
     W1 = parameters["W1"]
@@ -58,11 +60,11 @@ def feedforward(Xtrain,parameters):
     cache = {"z1" : z1, "a1" : a1, "z2" : z2, "a2" : a2}
     return cache
 
-def calculate_grads(cache,Ytrain,m,W1,W2,X):
+def calculate_grads(cache,Ytrain,m,parameters,X):
     dA2 = (cache["a2"] - Ytrain)
     dW2 = (2/m) * np.dot((dsigmoid(cache["z2"]) * dA2), np.transpose(cache["a1"]))
     db2 = (2/m) * np.sum(dsigmoid(cache["z2"]) * dA2)
-    dA1 = np.dot(W2.T, dA2) * dsigmoid(cache["z2"])
+    dA1 = np.dot(parameters["W2"].T, dA2) * dsigmoid(cache["z2"])
     dW1 = (2/m) * np.dot((drelu(cache["z1"]) * dA1), np.transpose(X))
     db1 = (2/m) * np.sum(drelu(cache["z1"]) * dA1)
     grads = {"dW1" : dW1, "db1" : db1, "dW2" : dW2, "db2" : db2}
@@ -84,19 +86,36 @@ def calculate_accuracy(a2,Ytrain,m):
     acc = np.sum(acc) / m
     return acc
 
+def test_model(Xtest,Ytest,parameters,m):
+    cache = feedforward(Xtest,parameters)
+    print("Cost: " + str(calculate_cost(cache["a2"],Ytest,m)))
+    print("Test Accuracy:" + str(calculate_accuracy(cache["a2"],Ytest,m)))
 
-def train_neural_network(Xtrain,Ytrain):
+def train_neural_network(Xtrain,Ytrain,Xtest,Ytest):
     alpha = 0.5
     iterations = 10000
-    m = np.shape(Ytrain)[1]
-    W1, W2, b1, b2 = initialise_weights(3,np.shape(Xtrain)[0],1)
-    parameters = {"W1" : W1, "W2" : W2, "b1" : b1, "b2": b2}
+    mtrain = np.shape(Ytrain)[1]
+    mtest = np.shape(Ytest)[1]
+    parameters = initialise_weights(3,np.shape(Xtrain)[0],1)
     for i in range(0,iterations):
         cache = feedforward(Xtrain,parameters)
-        grads = calculate_grads(cache,Ytrain,m,W1,W2,Xtrain)
+        grads = calculate_grads(cache,Ytrain,mtrain,parameters,Xtrain)
         parameters = adjust_weights(parameters,alpha,grads)
-        print(calculate_accuracy(cache["a2"],Ytrain,m))
-        print(calculate_cost(cache["a2"],Ytrain,m))
+        print(calculate_cost(cache["a2"],Ytrain,mtrain))
+    print("Training Accuracy:" + str(calculate_accuracy(cache["a2"],Ytrain,mtrain)))
+    test_model(Xtest,Ytest,parameters,mtest)
+
+def train_test_split(X,Y):
+    X = X.values
+    Y = Y.values
+    Y = Y.reshape(np.shape(Y)[0],1)
+    Y = np.transpose(Y)
+    X = np.transpose(X)
+    Xtrain = X[:,0:741]
+    Xtest = X[:,741:]
+    Ytrain = Y[:,0:741]
+    Ytest = Y[:,741:]
+    return Xtrain, Xtest, Ytrain, Ytest
 
 def main():
     train = read_in_data()
@@ -105,12 +124,8 @@ def main():
     map_categories(Xtrain)
     complete_age_data(Xtrain)
     feature_normalisation(Xtrain)
-    Xtrain = Xtrain.values
-    Ytrain = Ytrain.values
-    Ytrain = Ytrain.reshape(np.shape(Ytrain)[0],1)
-    Ytrain = np.transpose(Ytrain)
-    Xtrain = np.transpose(Xtrain)
-    train_neural_network(Xtrain,Ytrain)
+    Xtrain, Xtest, Ytrain, Ytest = train_test_split(Xtrain,Ytrain)
+    train_neural_network(Xtrain,Ytrain,Xtest,Ytest)
 
 class NeuralNetwork(object):
 
